ff51d0d536190a69ed87d7bfc7ce2f92de3c7531
Jeremías ramírez <30301246+jeremiasramirez@users.noreply.github.com>
2022-06-07 16:34:12 -0400

    Add files via upload
    subida forzada

---
+++ practica02/practica02/FrmCalculadora.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace practica02
+{
+    partial class FrmCalculadora
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.operacion = new System.Windows.Forms.ComboBox();
+            this.resultado = new System.Windows.Forms.TextBox();
+            this.txtValor2 = new System.Windows.Forms.TextBox();
+            this.txtValor1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.calcular = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel1.Controls.Add(this.operacion);
+            this.panel1.Controls.Add(this.resultado);
+            this.panel1.Controls.Add(this.txtValor2);
+            this.panel1.Controls.Add(this.txtValor1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(89, 41);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(402, 251);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // operacion
+            // 
+            this.operacion.FormattingEnabled = true;
+            this.operacion.Items.AddRange(new object[] {
+            "Sumar",
+            "Restar",
+            "Multiplicar",
+            "Dividir"});
+            this.operacion.Location = new System.Drawing.Point(136, 139);
+            this.operacion.Name = "operacion";
+            this.operacion.Size = new System.Drawing.Size(209, 28);
+            this.operacion.TabIndex = 8;
+            this.operacion.SelectedIndexChanged += new System.EventHandler(this.operacion_SelectedIndexChanged);
+            // 
+            // resultado
+            // 
+            this.resultado.BackColor = System.Drawing.Color.White;
+            this.resultado.Enabled = false;
+            this.resultado.Font = new System.Drawing.Font("Microsoft Tai Le", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultado.ForeColor = System.Drawing.Color.White;
+            this.resultado.Location = new System.Drawing.Point(177, 197);
+            this.resultado.Name = "resultado";
+            this.resultado.Size = new System.Drawing.Size(116, 35);
+            this.resultado.TabIndex = 7;
+            this.resultado.Text = "0";
+            this.resultado.TextChanged += new System.EventHandler(this.resultado_TextChanged);
+            // 
+            // txtValor2
+            // 
+            this.txtValor2.Location = new System.Drawing.Point(136, 87);
+            this.txtValor2.Name = "txtValor2";
+            this.txtValor2.Size = new System.Drawing.Size(209, 26);
+            this.txtValor2.TabIndex = 5;
+            // 
+            // txtValor1
+            // 
+            this.txtValor1.Location = new System.Drawing.Point(136, 30);
+            this.txtValor1.Name = "txtValor1";
+            this.txtValor1.Size = new System.Drawing.Size(209, 26);
+            this.txtValor1.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.label4.Location = new System.Drawing.Point(16, 197);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Resultado";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.label3.Location = new System.Drawing.Point(16, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Operacion";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.label2.Location = new System.Drawing.Point(29, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Valor 2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.label1.Location = new System.Drawing.Point(29, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valor 1";
+            // 
+            // calcular
+            // 
+            this.calcular.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.calcular.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calcular.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.calcular.Location = new System.Drawing.Point(167, 323);
+            this.calcular.Name = "calcular";
+            this.calcular.Size = new System.Drawing.Size(236, 46);
+            this.calcular.TabIndex = 1;
+            this.calcular.Text = "&Calcular";
+            this.calcular.UseVisualStyleBackColor = false;
+            this.calcular.Click += new System.EventHandler(this.calcular_Click);
+            // 
+            // FrmCalculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(578, 394);
+            this.Controls.Add(this.calcular);
+            this.Controls.Add(this.panel1);
+            this.MaximumSize = new System.Drawing.Size(600, 450);
+            this.MinimumSize = new System.Drawing.Size(600, 450);
+            this.Name = "FrmCalculadora";
+            this.Text = "Calculadora";
+            this.Load += new System.EventHandler(this.FrmCalculadora_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox operacion;
+        private System.Windows.Forms.TextBox resultado;
+        private System.Windows.Forms.TextBox txtValor2;
+        private System.Windows.Forms.TextBox txtValor1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button calcular;
+    }
+}
+
---
+++ practica02/practica02/FrmCalculadora.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practica02
+{
+    public partial class FrmCalculadora : Form
+    {
+        public FrmCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void FrmCalculadora_Load(object sender, EventArgs e)
+        {
+            operacion.SelectedIndex = 0;
+        }
+
+        private void calcular_Click(object sender, EventArgs e)
+        {
+            if (operacion.SelectedIndex == 0 || operacion.SelectedIndex == 1 || operacion.SelectedIndex == 2 || operacion.SelectedIndex == 3)
+            {
+    
+                
+           
+            
+            try
+            {
+                if (operacion.SelectedIndex == 0) resultado.Text = (double.Parse(txtValor1.Text) + double.Parse(txtValor2.Text)).ToString();
+
+                if (operacion.SelectedIndex == 1) resultado.Text = (double.Parse(txtValor1.Text) - double.Parse(txtValor2.Text)).ToString();
+
+                if (operacion.SelectedIndex == 2) resultado.Text = (double.Parse(txtValor1.Text) * double.Parse(txtValor2.Text)).ToString();
+
+                if (operacion.SelectedIndex == 3) resultado.Text = (double.Parse(txtValor1.Text) / double.Parse(txtValor2.Text)).ToString();
+              
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("No se pudo realizar la operacion", "Formato incorrecto");
+            }
+           }
+            else { operacion.SelectedIndex = 0; }
+        }
+
+        private void operacion_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void resultado_TextChanged(object sender, EventArgs e)
+        {
+         
+        }
+    }
+}
